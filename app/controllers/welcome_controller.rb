bc4459301134f3f830657549f146efc116e1bb53
tanihito <tanihito7@gmail.com>
2015-03-21 20:30:17 +0900

    create result page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,8 @@ class WelcomeController < ApplicationController
       {name: 'ちはやふる', url: 'http://yuzuya.style.coocan.jp/blog/img/2012/1023/00.jpg'}
     ]
   end
+
+  def result
+    p params
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Bukureco::Application.routes.draw do
   get "welcome/index"
   get "welcome/index2"
-
+  match '/welcome/result' => "welcome#result"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -51,7 +51,7 @@ Bukureco::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'welcome/index'
 
   # See how all your routes lay out with "rake routes"
 
