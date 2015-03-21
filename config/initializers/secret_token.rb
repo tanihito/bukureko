d75108c4d270ce0a54e6bc9a1f2054da5b59ee95
tanihito <tanihito7@gmail.com>
2015-03-21 16:30:25 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bukureco::Application.config.secret_token = 'be65d7ab389c74ef1cbafe6368b23e7d84c4ea5d663c5484f85b89d3b6ac54478fbffaebd6ea9112d32efe15aef8359337fe27764d2e3c9e3a70ac53f93cb93a'
